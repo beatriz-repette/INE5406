5fa50817db81b17864af3f8538fee58a598a9c54
Bea <beatriz.repette@gmail.com>
2024-10-25 13:54:57 -0300

    Create somador.vhd

---
+++ Atividade2/somador.vhd
@@ -0,0 +1,18 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY somador IS
+	GENERIC (N : POSITIVE := 7);
+	PORT (
+		a: IN std_logic_vector (N-1 downto 0);
+		b: IN std_logic_vector (N-1 downto 0);
+		s: OUT std_logic_vector (N-1 DOWNTO 0)
+	);
+END somador;
+
+ARCHITECTURE behavior OF somador IS
+BEGIN
+	s <= a + b;
+
+end behavior;
