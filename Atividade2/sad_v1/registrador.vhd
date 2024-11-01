3a77394df07bcf9757fa89049803de25bcdb3c5b
Bea <beatriz.repette@gmail.com>
2024-11-01 15:57:50 -0300

    creating sad_v1

---
+++ Atividade2/sad_v1/registrador.vhd
@@ -0,0 +1,29 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY registrador IS
+	GENERIC (N : POSITIVE := 8);
+	PORT (
+		clk: IN std_logic;
+		rst: IN std_logic;
+		a: IN std_logic_vector (N-1 downto 0);
+		b: OUT std_logic_vector (N-1 downto 0)
+	);
+END registrador;
+
+ARCHITECTURE behavior OF registrador IS
+	signal reg_value : std_logic_vector(N-1 downto 0);
+BEGIN
+	process(clk, rst)
+	BEGIN
+	if rst = '1' then
+		reg_value <= (others => '0');
+	elsif rising_edge(clk) then
+		reg_value <= a;
+	end if;
+	end process;
+	
+	b <= reg_value;
+
+end behavior;
