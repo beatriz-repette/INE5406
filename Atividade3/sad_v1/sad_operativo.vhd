600569f3c5c5263db53c32502f08b6b7e088d8ad
Bea <beatriz.repette@gmail.com>
2024-11-18 09:26:38 -0300

    num bits em função de B

--- Atividade3/sad_v1/sad_operativo.vhd
+++ Atividade3/sad_v1/sad_operativo.vhd
@@ -3,12 +3,13 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 entity sad_operativo is
+    generic (B: positive);
     port (
-		  sample_ori : IN STD_LOGIC_VECTOR (7 DOWNTO 0); -- Mem_A[end]
-		  sample_can : IN STD_LOGIC_VECTOR (7 DOWNTO 0); -- Mem_B[end]
+		  sample_ori : IN STD_LOGIC_VECTOR (B-1 DOWNTO 0); -- Mem_A[end]
+		  sample_can : IN STD_LOGIC_VECTOR (B-1 DOWNTO 0); -- Mem_B[end]
 		  clk, zi, ci, cpa, cpb, zsoma, csoma, csad_reg, reset : in std_logic;
 		  menor : out std_logic;
-		  sad_value : OUT STD_LOGIC_VECTOR (13 DOWNTO 0); -- SAD
+		  sad_value : OUT STD_LOGIC_VECTOR (B+5 DOWNTO 0); -- SAD
 		  address : OUT STD_LOGIC_VECTOR (5 DOWNTO 0) -- end
 
     );
@@ -18,7 +19,7 @@ architecture arch of sad_operativo is
 	
 	-- COMPONENTES -- 
 	component mux is
-		GENERIC (N : POSITIVE := 7);
+		GENERIC (N : POSITIVE := B-1);
 		PORT (
 			sel: IN std_logic;
 			w0, w1: IN std_logic_vector (N-1 downto 0);
@@ -27,7 +28,7 @@ architecture arch of sad_operativo is
 	END component;
 	
 	component registrador is
-		GENERIC (N : POSITIVE := 8);
+		GENERIC (N : POSITIVE := B);
 		PORT (
 			clk: IN std_logic;
 			rst: IN std_logic;
@@ -38,7 +39,7 @@ architecture arch of sad_operativo is
 	END component;
 	
 	component somador is
-		GENERIC (N : POSITIVE := 7);
+		GENERIC (N : POSITIVE := B-1);
 		PORT (
 			a: IN std_logic_vector (N-1 downto 0);
 			b: IN std_logic_vector (N-1 downto 0);
@@ -47,7 +48,7 @@ architecture arch of sad_operativo is
 	END component;
 	
 	component subtrator is
-		GENERIC (N : POSITIVE := 7);
+		GENERIC (N : POSITIVE := B-1);
 		PORT (
 			a: IN std_logic_vector (N-1 downto 0);
 			b: IN std_logic_vector (N-1 downto 0);
@@ -58,11 +59,11 @@ architecture arch of sad_operativo is
 ----- Sinais Internos -----
 
 -- Sinais circuitinho
-    signal s1, s2, sfinal: std_logic_vector (6 downto 0);
+    signal s1, s2, sfinal: std_logic_vector (B-2 downto 0);
 
 -- Sinais circuitao
-    signal pa, pb, sub: std_logic_vector (7 downto 0);
-    signal sum, smux, sreg, finall: std_logic_vector (13 downto 0);
+    signal pa, pb, sub: std_logic_vector (B-1 downto 0);
+    signal sum, smux, sreg, finall: std_logic_vector (B+5 downto 0);
 
 
 ----- PortMap -----
@@ -71,47 +72,47 @@ architecture arch of sad_operativo is
 
 	BEGIN
     mux_i : mux
-    generic map (N => 7)
-    port map(zi, sfinal, "0000000", s1);
+    generic map (N => B-1)
+    port map(zi, sfinal, std_LOGIC_VECTOR(resize(unsigned('0'), B-1)), s1);
     
     reg_i : registrador
-    generic map (N => 7)
+    generic map (N => B-1)
     port map(clk, reset, ci, s1, s2);
     
-    menor <= not(s2(6));
-    address <= s2(5 downto 0);
+    menor <= not(s2(B-2));
+    address <= s2(B-3 downto 0);
     
     somador_i : somador
-    generic map (N => 7)
-    port map(s2, "0000001", sfinal);
+    generic map (N => B-1)
+    port map(s2, std_LOGIC_VECTOR(resize(unsigned('1'), B-1)), sfinal);
     
 --PortMap Circuitao
     reg_pa : registrador
-    generic map (N => 8)
+    generic map (N => B)
     port map (clk, reset, cpa, sample_ori, pa);
     
     reg_pb : registrador
-    generic map (N => 8)
+    generic map (N => B)
     port map(clk, reset, cpb, sample_can, pb);
     
     abs_subtrator : subtrator
-    generic map (N => 9)
-    port map(std_LOGIC_VECTOR(resize(unsigned(pa), 9)), std_LOGIC_VECTOR(resize(unsigned(pb), 9)), sub);
+    generic map (N => B+1)
+    port map(std_LOGIC_VECTOR(resize(unsigned(pa), B+1)), std_LOGIC_VECTOR(resize(unsigned(pb), B+1)), sub);
     
     soma : somador
-    generic map (N => 14)
-    port map(sreg, (std_LOGIC_VECTOR(resize(unsigned(sub), 14))), sum);
+    generic map (N => B+6)
+    port map(sreg, (std_LOGIC_VECTOR(resize(unsigned(sub), B+6))), sum);
     
     muxx: mux
-    generic map (N => 14)
-    port map(zsoma, sum, "00000000000000", smux);
+    generic map (N => B+6)
+    port map(zsoma, sum, std_LOGIC_VECTOR(resize(unsigned('0'), B+6)), smux);
     
     antepenultimo_reg : registrador
-    generic map(N => 14)
+    generic map(N => B+6)
     port map(clk, reset, csoma, smux, sreg);
     
     ultimp_reg : registrador
-    generic map (N => 14)
+    generic map (N => B+6)
     port map(clk, reset, csad_reg, sreg, finall);
     
     sad_value <= finall;
