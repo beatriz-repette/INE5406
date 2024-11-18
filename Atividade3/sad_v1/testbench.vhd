4dde79c0c4ea2ea16a42715574ac1376b08e2a3e
ana luiza <analuizarguima@gmail.com>
2024-11-18 15:21:48 -0300

    Update testbench.vhd

--- Atividade3/sad_v1/testbench.vhd
+++ Atividade3/sad_v1/testbench.vhd
@@ -10,15 +10,18 @@ end testbench;
 
 architecture tb of testbench is
 
-  clk : STD_LOGIC; 
-  enable : STD_LOGIC; 
-  reset : STD_LOGIC; 
-  sample_ori : STD_LOGIC_VECTOR (7 DOWNTO 0); 
-  sample_can : STD_LOGIC_VECTOR (7 DOWNTO 0); 
-  read_mem : STD_LOGIC; 
-  address : STD_LOGIC_VECTOR (5 DOWNTO 0); 
-  sad_value : STD_LOGIC_VECTOR (13 DOWNTO 0); 
-  done: STD_LOGIC;
+
+  signal clk : STD_LOGIC; 
+  signal enable : STD_LOGIC; 
+  signal reset : STD_LOGIC; 
+  signal sample_ori : STD_LOGIC_VECTOR (7 DOWNTO 0); 
+  signal sample_can : STD_LOGIC_VECTOR (7 DOWNTO 0); 
+  signal read_mem : STD_LOGIC; 
+  signal address : STD_LOGIC_VECTOR (5 DOWNTO 0); 
+  signal sad_value : STD_LOGIC_VECTOR (13 DOWNTO 0); 
+  signal done: STD_LOGIC;
+  
+
 
 CONSTANT passo : TIME := 20 ns;	 
 
@@ -38,7 +41,7 @@ begin
     wait for 9*passo;
     sample_ori <= std_logic_vector(to_unsigned(0, sample_ori'length));
     sample_can <= std_logic_vector(to_unsigned(1, sample_can'length));
-    assert(sad_value='00000001000000')
+    assert(sad_value="00000001000000")
     report "Falha na simulação" severity error; 
 
     wait for 20 ns;
